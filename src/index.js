877be2d4428bb8344b4a1e8553647cc3d3c43669
aminos02 <azzouzamine02@gmail.com>
2021-01-15 17:20:49 +0100

    4h24

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./index.css";
-import { books } from "./books";
-import Book from "./Book";
-function BookList() {
-  return (
-    <section className="booklist">
-      {books.map((book) => (
-        <Book key={book.id} {...book}></Book>
-      ))}
-    </section>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDom.render(<BookList />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
