a6028699ba476c8ce5d3496799abd98bb57e0247
aminos02 <azzouzamine02@gmail.com>
2021-01-17 22:24:33 +0100

    Propos

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+const book = {
+  title: "Habits",
+  author: "Hamouda be 9ouda",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._AC_SX184_.jpg",
+};
 function BookList() {
   return (
-    <section class="booklist">
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book {...book} />
+      <Book title={book.title} author={book.author} img={book.img} />
       <Book />
     </section>
   );
 }
-var title = "Atomic Habits";
-var author = "azzouz amin";
-const Book = () => {
+
+const Book = (arg1) => {
+  console.log(arg1);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._AC_SX184_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={arg1.img} alt="" />
+      <h1>{arg1.title}</h1>
+      <h4>{arg1.author}</h4>
     </article>
   );
 };
